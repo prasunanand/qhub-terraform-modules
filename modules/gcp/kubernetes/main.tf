c0c72690a0e44f442def995e042dcdb2fd6b8ae0
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-08-14 18:32:35 +0530

    remove old nodepool config

--- modules/gcp/kubernetes/main.tf
+++ modules/gcp/kubernetes/main.tf
@@ -108,41 +108,6 @@ resource "google_container_cluster" "main" {
   remove_default_node_pool = true
 }
 
-
-
-
-resource "google_container_node_pool" "main" {
-  count = length(var.node_groups)
-
-  name     = var.node_groups[count.index].name
-  location = var.location
-  cluster  = google_container_cluster.main.name
-
-  initial_node_count = min(var.node_groups[count.index].min_size, 1)
-
-  autoscaling {
-    min_node_count = var.node_groups[count.index].min_size
-    max_node_count = var.node_groups[count.index].max_size
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = var.node_groups[count.index].instance_type
-
-    service_account = google_service_account.main.email
-
-    oauth_scopes = local.node_group_oauth_scopes
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
-
 /******************************************
   Create Container Cluster node pools
  *****************************************/
