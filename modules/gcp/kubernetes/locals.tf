0b7a38e2d99b03f1be4f4c7d91a80f3a14438c81
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-08-17 12:28:56 +0530

    Moving locals around

--- modules/gcp/kubernetes/locals.tf
+++ modules/gcp/kubernetes/locals.tf
@@ -1,20 +1,5 @@
 
 
-/******************************************
-  Get available zones in region
- *****************************************/
-data "google_compute_zones" "available" {
-  provider = google
-
-  project = var.project_id
-  region  = local.region
-}
-
-resource "random_shuffle" "available_zones" {
-  input        = data.google_compute_zones.available.names
-  result_count = 3
-}
-
 locals {
   node_group_service_account_roles = concat(var.additional_node_group_roles, [
     "roles/logging.logWriter",
@@ -28,23 +13,6 @@ locals {
     "https://www.googleapis.com/auth/monitoring"
   ])
 
-}
-
-/******************************************
-  Get available container engine versions
- *****************************************/
-data "google_container_engine_versions" "region" {
-  location = local.location
-  project  = var.project_id
-}
-
-data "google_container_engine_versions" "zone" {
-  // Work around to prevent a lack of zone declaration from causing regional cluster creation from erroring out due to error
-  //
-  //     data.google_container_engine_versions.zone: Cannot determine zone: set in this resource, or set provider-level zone.
-  //
-  location = local.zone_count == 0 ? data.google_compute_zones.available.names[0] : var.zones[0]
-  project  = var.project_id
 }
 
 
