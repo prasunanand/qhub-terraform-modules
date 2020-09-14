b6f00d17c409c558ab02416252a2ab0e42af4588
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-09-14 18:02:43 +0530

    disable private endpoint

--- modules/gcp/beta-private-cluster/cluster.tf
+++ modules/gcp/beta-private-cluster/cluster.tf
@@ -228,7 +228,7 @@ resource "google_container_cluster" "primary" {
   dynamic "private_cluster_config" {
     for_each = var.enable_private_nodes ? [{
       enable_private_nodes    = var.enable_private_nodes,
-      enable_private_endpoint = var.enable_private_endpoint
+      enable_private_endpoint = false
       master_ipv4_cidr_block  = var.master_ipv4_cidr_block
     }] : []
 
